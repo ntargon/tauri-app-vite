f698ad5012158fc69c0fd68c12644a455bd91887
ntargon <argon19951128@gmail.com>
2022-05-18 00:05:10 +0900

    wasm

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import init, { greet, start } from 'wasm'
+
+const hoge = () => {
+    init()
+        .then((_)=> {
+            greet();
+            start();
+        })
+};
 </script>
 
 <template>
 <h1>Canvas</h1>
+<button type="button" @click="hoge">press</button>
 <canvas id="canvas" width="400" height="400"></canvas>
 </template>
 
